361a5da6c280ad80ac653b0e588948bad9ee7749
kunou <t-kunou@esm.co.jp>
2018-08-21 12:26:20 +0900

    Show members

--- src/whowhere/core.cljs
+++ src/whowhere/core.cljs
@@ -1,6 +1,14 @@
 (ns whowhere.core
   (:require [whowhere.entry-points]))
 
+(defn members []
+  (->> (.. js/SpreadsheetApp
+      getActiveSheet
+      getDataRange
+      getValues)
+    array-seq
+    (map array-seq)))
+
 (defn ^:export start []
-  (+ 1 2))
+  (members))
 
