b4b8c1c8711ebc27f93b870801c7583c5170d4b7
kunou <t-kunou@esm.co.jp>
2018-09-19 11:40:56 +0900

    Add loading

--- view/index.clj
+++ view/index.clj
@@ -44,24 +44,34 @@
       :width "50px"
       :height "50px"
     }]
+    [:.unvisible {
+      :display "none"
+    }]
+    [:.force_visible {
+      :display "block !important"
+    }]
   ))
 
 (def index-html
   (html5
     (html
       [:head
+       [:script {:defer nil :src "https://use.fontawesome.com/releases/v5.3.1/js/all.js" :integrity "sha384-kW+oWsYx3YpxvjtZjFXqazFpA7UP/MbiY4jvs+RWZo2+N94PFZ36T6TFkc9O3qoB" :crossorigin "anonymous"}]
         [:script {:src "https://cdn.jsdelivr.net/npm/vue/dist/vue.js"}]
         [:base {:target "_top"}]]
         [:style styles]
       [:body
-        [:div {:id "projects"} 
-          [:div {:v-for "project in projects" :class "project"}
-            [:p "{{ project['name'] }}"]
-            [:ul 
-              [:li {:v-for "member in project['members']" :class "member"}
-                [:p "{{ member['name'] }}"]
-                [:img {"v-bind:src" "member['icon']" :class "icon"}
-                [:p "{{ member['location'] }}"]]]]]]
+        [:div {:id "visible"}
+          [:i {:class "fa fa-spinner fa-pulse fa-3x fa-fw" "v-bind:class" "{unvisible: active}"}]
+          [:div {:class "unvisible" "v-bind:class" "{force_visible: active}"} 
+            [:div {:id "projects"}
+              [:div {:v-for "project in projects" :class "project"}
+                [:p "{{ project['name'] }}"]
+                [:ul
+                  [:li {:v-for "member in project['members']" :class "member"}
+                    [:p "{{ member['name'] }}"]
+                    [:img {"v-bind:src" "member['icon']" :class "icon"}
+                    [:p "{{ member['location'] }}"]]]]]]]]
         [:script "
           google.script.run.withSuccessHandler(initializeVue).calc();
           function initializeVue(projects) {
@@ -69,6 +79,11 @@
               el: '#projects',
               data: {
                 projects: projects
+              }})
+            new Vue({
+              el: '#visible',
+              data: {
+                active: true
               }})
             } 
         "]])))
